401fb1560a20f7aab2775bc5c615863119448138
羅仲宏 <chunghung@hungs-MacBook-Pro.local>
2022-04-12 23:11:13 +0800

    vue graphql done

--- node_modules/@octokit/graphql-schema/test/index.js
+++
@@ -1,2 +0,0 @@
-require("./smoke");
-require("./validate");
--- node_modules/@octokit/graphql-schema/test/smoke.js
+++
@@ -1,7 +0,0 @@
-const assert = require("assert");
-
-const main = require("../");
-
-assert.strict.equal(typeof main.validate, "function");
-assert.strict.equal(typeof main.schema.json, "object");
-assert.strict.equal(typeof main.schema.idl, "string");
