863da769de5e1db285fceae0425623f22a27d1b1
羅仲宏 <chunghung@hungs-MacBook-Pro.local>
2022-04-14 22:37:46 +0800

    vue graphql practice

--- frontend/src/views/aboutView.vue
+++ frontend/src/views/aboutView.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="about">
         <h1>This is an project detail page</h1>
-        <div v-if="result && result.building">
-            <h2 v-for=" building in result.building" :key="building.id">{{ building.id }},{{ building.name }}</h2>
+        <div v-if="buildings">
+            <h2 v-for=" building in buildings" :key="building.id">{{ building.id }},{{ building.name }}</h2>
         </div>
+        <div v-else-if="error">Error: {{ error.message }}</div>
     </div>
 </template>
 
@@ -15,21 +16,25 @@
 //     getBuilding
 // } from '../schemas/building'
 import {
-    useQuery
+    useQuery, useResult
 } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 export default {
     setup () {
-        const { result } = useQuery(gql`
-    query building {
+        const { result, loading, error } = useQuery(gql`
+    query getbuilding {
         building {
             id
             name
         }
     }
 `)
+
+        const buildings = useResult(result, null, data => data.building)
         return {
-            result
+            buildings,
+            loading,
+            error
         }
     }
 }
