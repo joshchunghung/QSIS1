0e7e4d1bc2e79b21c5b0858de6fc9a721aa5926a
joshchunghung <joshchunghung@gmail.com>
2022-04-18 18:44:11 +0800

    新增middleware

--- backend/models.py
+++ backend/models.py
@@ -123,7 +123,7 @@ class DjangoSession(models.Model):
 
 
 class Building(models.Model):
-    id = models.IntegerField(primary_key=True,auto_created=True, editable=False)  # Field name made lowercase.
+    id = models.IntegerField(db_column="buildingID",primary_key=True,auto_created=True, editable=False)  # Field name made lowercase.
     #id = models.UUIDField(primary_key=True, default=uuid4(), auto_created=True, editable=False)
     name = models.CharField(max_length=255, blank=True, null=True)  # Field name made lowercase.
     abbreviation = models.CharField(max_length=10, blank=True, null=True)
@@ -143,7 +143,7 @@ class Building(models.Model):
         db_table = 'building'
         
 class Station(models.Model):
-    id = models.IntegerField(primary_key=True,auto_created=True, editable=False)
+    id = models.IntegerField(db_column="stationID",primary_key=True,auto_created=True, editable=False)
     SN= models.IntegerField(blank=False)
     code=models.CharField(max_length=5, blank=True, null=True)
     building=models.CharField(max_length=10, blank=False, null=True)
